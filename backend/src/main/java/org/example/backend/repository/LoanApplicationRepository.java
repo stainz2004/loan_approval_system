572606c225747ff6546ebdfea7678f491a052527
SanderR <SanderR999@gmail.com>
2026-04-08 21:12:02 +0300

    Add method to retrieve loan applications by status in LoanApplicationRepository

--- backend/src/main/java/org/example/backend/repository/LoanApplicationRepository.java
+++ backend/src/main/java/org/example/backend/repository/LoanApplicationRepository.java
@@ -1,10 +1,15 @@
 package org.example.backend.repository;
 
 
+import org.example.backend.dto.LoanApplicationStatus;
 import org.example.backend.entity.LoanApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LoanApplicationRepository extends JpaRepository<LoanApplication, Long> {
+
+    List<LoanApplication> findByLoanApplicationStatus(LoanApplicationStatus status);
 }
